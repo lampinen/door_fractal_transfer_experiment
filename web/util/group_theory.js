202aa2a91301817b2442108a615b5e6c9cfed3b3
Andrew Lampinen <lampinen@stanford.edu>
2017-11-15 22:41:50 -0800

    Dicyclic, dihedral

--- web/util/group_theory.js
+++ web/util/group_theory.js
@@ -16,12 +16,90 @@ function cyclic_group(order) {
     this.elements = range(order);
 }
 
-cyclic_group.prototype.get_order() = function() {
+cyclic_group.prototype.get_order = function() {
     return this.order;
 }
+
+cyclic_group.prototype.get_elements = function() {
+    return this.elements;
+}
+
 
 cyclic_group.prototype.operation = function(x, y) {
     return (x + y) % this.order;
 }
 
 
+
+// Dicyclic, where each element x is represented in a canonical form such that
+// int(x / 4) s-applications followed by (x % 4) r-applications, s & r being
+// group generators
+function dicyclic_group_12 () {
+    this.order = 12;
+    this.elements = range(12);
+}
+
+dicyclic_group_12.prototype.get_order = function() {
+    return this.order;
+}
+
+dicyclic_group_12.prototype.get_elements = function() {
+    return this.elements;
+}
+
+dicyclic_group_12.prototype.operation = function(x, y) {
+    var rx = x % 4;
+    var sx = Math.floor(x / 4);
+    var ry = y % 4;
+    var sy = Math.floor(y / 4);
+
+    var rres = 0;
+    var sres = 0;
+    if (sx == 0) {
+        rres = (rx + ry) % 4;
+        sres = sy;
+    } else if (ry % 2 == 0) { // commutes
+        rres = (rx + ry) % 4;
+        sres = (sx + sy) % 3;
+    } else {
+        rres = (rx + ry) % 4;
+        sres = (sy + 3 - sx) % 3;
+    }
+    return rres + 4 * sres;
+}
+
+
+// Dihedral, where each element x is represented in a canonical form such that
+// int(x / n) s-applications followed by (x % n) r-applications, s & r being
+// group generators (flip and rotation, respectively)
+function dihedral_group (num_sides) {
+    this.num_sides = num_sides;
+    this.order = num_sides*2;
+    this.elements = range(num_sides*2);
+}
+
+dihedral_group.prototype.get_order = function() {
+    return this.order;
+}
+
+dihedral_group.prototype.get_elements = function() {
+    return this.elements;
+}
+
+dihedral_group.prototype.operation = function(x, y) {
+    var rx = x % this.num_sides;
+    var sx = Math.floor(x / this.num_sides);
+    var ry = y % this.num_sides;
+    var sy = Math.floor(y / this.num_sides);
+
+    var rres = 0;
+    var sres = 0;
+    if (sx == 0) {
+        rres = (rx + ry) % this.num_sides;
+        sres = sy;
+    } else {
+        rres = (rx + this.num_sides - ry) % this.num_sides;
+        sres = (sx + sy) % 2;
+    }
+    return rres + this.num_sides * sres;
+}
