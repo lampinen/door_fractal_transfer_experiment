16f7fe5de9d04303b508473cf5f789195e0d6ed9
Andrew Lampinen <lampinen@stanford.edu>
2018-02-26 17:06:38 -0800

    stimulus generation

---
+++ stimulus_generation/patterns.py
@@ -0,0 +1,22 @@
+"""Generating patterns of location visits for the experiment"""
+from numpy.random import shuffle
+num_locations = 8
+num_trials = 50
+
+num_perms = (num_trials // num_locations) + 1
+
+
+blah = [range(num_locations) for i in xrange(num_perms)]
+[shuffle(x) for x in blah] # in place
+blah = [x for y in blah for x in y]
+blah = blah[:num_trials+1]
+while any([blah[i] == blah[i+1] for i in xrange(num_trials)]):
+    blah = [range(num_locations) for i in xrange(num_perms)]
+    [shuffle(x) for x in blah] # in place
+    blah = [x for y in blah for x in y]
+    blah = blah[:num_trials+1]
+
+blah2 = ", ".join(["{start: %i, goal: %i}" %(blah[i], blah[i+1]) for i in xrange(num_trials)])
+print(blah2)
+
+
