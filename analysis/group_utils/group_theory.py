a61018521be126fe7b24a842c2b80a436fc2ace6
Andrew Lampinen <lampinen@stanford.edu>
2018-03-01 13:04:19 -0800

    python versions of non-groups

--- analysis/group_utils/group_theory.py
+++ analysis/group_utils/group_theory.py
@@ -55,3 +55,76 @@ class dicyclic_group_12(object):
         return rres + 4 * sres
 
 
+# Non group structures
+
+class square_cycles(object):
+    def __init__(self):
+        self.order = 8 
+
+    def get_some_generators(self):
+        return [0, 1]
+
+    def operation(self, x, y):
+        if (y not in [0, 1]):
+            raise ValueError("Square cycle operation error!")
+
+	if (x < 4): # lower cycle
+	    if (y == 0):
+		res = (x + 1) % 4;
+	    else:
+		if ((x % 2) == 1):
+		    res = x - 1;
+		else:
+		    res = x + 4;
+	    
+	else: #upper cycle
+	    if (y == 0):
+		res = 4 + ((x - 1) % 4);
+	    else:
+		if ((x % 2) == 0):
+		    res = x + 1;
+		else:
+		    res = x - 4;
+		
+	return res 
+	
+
+class tr_cycles(object):
+    def __init__(self):
+        self.order = 8 
+
+    def get_some_generators(self):
+        return [0, 1]
+
+    def operation(self, x, y):
+        if (y not in [0, 1]):
+            raise ValueError("Square cycle operation error!")
+
+        if (y == 1):
+            if (x == 0):
+                res = 3;
+            else if (x == 3):
+                res = 4;
+            else if (x == 6):
+                res = 5;
+            else if (x == 5):
+                res = 2;
+            else:
+                res = (x + 4) % 8;
+            
+        else: # y == 0
+            if (x == 0):
+                res = 2;
+            else if (x == 6):
+                res = 4;
+            else if (x == 1 || x == 2):
+                res = x - 1;
+            else if (x == 4):
+                res = 7;
+            else if (x == 7):
+                res = 6;
+            else:
+                res = (x + 4) % 8;
+		
+	return res 
+
