82b1b38cbd35e4836d4c3d4b59290485f3120c6f
Andrew Lampinen <lampinen@stanford.edu>
2018-05-09 13:46:13 -0700

    Even more analysis

--- analysis/group_utils/isomorphism_counting.py
+++ analysis/group_utils/isomorphism_counting.py
@@ -19,3 +19,21 @@ chance_correct_hb /= num_perms
 chance_correct_ht /= num_perms
 print(chance_correct_hb)
 print(chance_correct_ht)
+
+
+chance_correct_alignment_hb = 0.
+for perm_i in range(len(hb_targets)):
+    for perm_j in range(len(hb_targets)):
+        chance_correct_alignment_hb += sum([hb_targets[perm_i][k] == hb_targets[perm_j][k] for k in range(6)])
+chance_correct_alignment_hb /= len(hb_targets) **2
+
+print(chance_correct_alignment_hb)
+
+
+chance_correct_alignment_ht = 0.
+for perm_i in range(len(ht_targets)):
+    for perm_j in range(len(ht_targets)):
+        chance_correct_alignment_ht += sum([ht_targets[perm_i][k] == ht_targets[perm_j][k] for k in range(6)])
+chance_correct_alignment_ht /= len(ht_targets) **2
+
+print(chance_correct_alignment_ht)
