e0f16e43f08f5d97150ec5fa00200af58bc44cc1
Andrew Lampinen <lampinen@stanford.edu>
2018-06-15 15:54:23 -0700

    ex4 preliminary analyses

--- anonymized_data/anonymize.sh
+++ anonymized_data/anonymize.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-subdir=ex2
+subdir=ex4
 origin=../data
 target=.
 
-mkdir -p ${target}/${subdir}/{aux,s0,s1,s2}
+mkdir -p ${target}/${subdir}/{aux,s0,s1,s2,s3}
 
 i=1
 for f in ${origin}/${subdir}/aux/*.json
@@ -13,5 +13,6 @@ do
     cp ${origin}/${subdir}/s0/${filename} ${target}/${subdir}/s0/${i}.json
     cp ${origin}/${subdir}/s1/${filename} ${target}/${subdir}/s1/${i}.json
     cp ${origin}/${subdir}/s2/${filename} ${target}/${subdir}/s2/${i}.json
+    cp ${origin}/${subdir}/s3/${filename} ${target}/${subdir}/s3/${i}.json
     i=$((i+1))
 done
