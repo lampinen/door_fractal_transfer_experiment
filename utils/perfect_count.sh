6539b396e23b2f37341f424666285f867a1cbdce
Andrew Lampinen <lampinen@stanford.edu>
2018-07-19 07:01:58 -0700

    auto downloading

--- utils/perfect_count.sh
+++ utils/perfect_count.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-experiment=ex4_cont
-session=s0
+experiment=ex5
+session=s2
+
+cd ../mturk/${experiment}/
+for d in ${session}*; do
+    cd $d
+    python /home/andrew/Documents/grad/res/mturk/my_cosub/cosub/cosub/runner.py download -p
+    cd ..
+done
+cd ../../
 
 
-cd ../data/${experiment}/${session}/
+cd ./data/${experiment}/${session}/
 for f in *.json; do
     echo $f
     grep -o '\"perfect\":true' $f | wc -l
     grep ${f%.*} ../../../mturk/${experiment}/${session}*/production-results/*.json
 done
-cd ~-
+cd ../../../utils
